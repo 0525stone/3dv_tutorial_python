ce7567b4507da505bc96971f17ab4e8517a89113
0525stone <0525stone@gmail.com>
2021-11-16 10:37:37 +0900

    introduction of code

--- src/3d_reconstruction.py
+++ src/3d_reconstruction.py
@@ -1,5 +1,7 @@
 """
-reconstructing points by several images by finding Fundamental matrix
+reconstructing from 2d points to 3D points by several images by finding Fundamental matrix
+
+data : 직접 구할 필요가 있음...
 
 keyword : camera calibration, 3d reconstruction
 """
