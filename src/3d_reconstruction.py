76cfb72e18c668f5b80bf6a98f1dde21d90ca256
0525stone <0525stone@gmail.com>
2021-11-16 10:38:14 +0900

    code

--- src/3d_reconstruction.py
+++ src/3d_reconstruction.py
@@ -5,3 +5,14 @@ data : 직접 구할 필요가 있음...
 
 keyword : camera calibration, 3d reconstruction
 """
+# Libraries
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+img1_dir = '../data/sample4.jpeg'
+img2_dir = '../data/sample5.jpeg'
+
+img1 = cv2.imread(img1_dir)
+img2 = cv2.imread(img2_dir)
