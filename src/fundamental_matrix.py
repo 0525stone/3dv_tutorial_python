28b43b4cee09e2b4def6e592b89cb6ab8ce44d3f
deephigh <0525stone@gmail.com>
2021-12-06 14:17:04 +0900

    fundamental_matrix test by 'matches'

--- src/fundamental_matrix.py
+++ src/fundamental_matrix.py
@@ -23,6 +23,8 @@ def compute_orb_keypoints(filename):
     """
     # load image
     img = cv2.imread(filename)
+    img = cv2.pyrDown(img)
+    img = cv2.pyrDown(img)
 
     # create orb object
     orb = cv2.ORB_create()
@@ -68,6 +70,13 @@ def compute_fundamental_matrix(filename1, filename2):
     # compute keypoint matches using descriptor
     matches = brute_force_matcher(des1, des2)
 
+    #
+    output_image = cv2.drawMatches(img1, kp1, img2, kp2, matches[:15], None, flags=2)
+    cv2.imshow('Output image', output_image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+ # Fundamental matrices
     # extract points
     pts1 = []
     pts2 = []
@@ -82,8 +91,35 @@ def compute_fundamental_matrix(filename1, filename2):
     # Compute fundamental matrix
     F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_8POINT)
 
-    # match 된것 몇 개만 옮겨서 circle로 찍어보
 
+    # match 된것 몇 개만 옮겨서 circle로 찍어보기
+    print(f'matches count : {len(matches)}')
+    for idx, p in enumerate(matches[:10]):
+        # print(f'kp {type(kp1)}, p {type(p)}')  # kp1 : tuple, p : cv2.DMatch
+        # print(f'point idx : {p.trainIdx}')
+        # print(f'point {kp1[p.queryIdx]}')
+        print(f'kp1 green {kp1[p.queryIdx].pt}')
+        print(f'kp2 red {kp2[p.trainIdx].pt}')
+
+        x1 , y1 = kp1[p.queryIdx].pt
+        x2, y2 = kp2[p.trainIdx].pt
+        x1n = int(F[0][0] * x1 + F[1][0] * y1 + F[2][0])
+        y1n = int(F[0][1] * x1 + F[1][1] * y1 + F[2][1])
+        x2n = int(F[0][0] * x2 + F[1][0] * y2 + F[2][0])
+        y2n = int(F[0][1] * x2 + F[1][1] * y2 + F[2][1])
+
+        x1,x2,y1,y2 = int(x1),int(x2),int(y1),int(y2)
+        plt.plot(x1, y1, color='green', marker='o')
+        plt.plot(x2, y2, color='red', marker='o')
+        plt.plot([x1,x2], [y1,y2], color='blue')
+        plt.plot(x1n, y1n, color='green', marker='v')
+        plt.plot(x2n, y2n, color='red', marker='v')
+
+
+
+
+        print('-'*20)
+    plt.show()
 
     return F
 
@@ -94,28 +130,29 @@ def main():
     file_list = sorted(glob.glob(image_dir + '*.png'))
 
     # compute F matrix between two images
-    first_image_path = '../data/s04.jpeg'
-    second_image_path = '../data/s05.jpeg'
+    first_image_path = '../data/s09.jpeg'
+    second_image_path = '../data/s10.jpeg'
     # print(compute_fundamental_matrix(first_image_path, second_image_path))
     F = compute_fundamental_matrix(first_image_path, second_image_path)
-    print(f'Fundamental matrix\n{F}')
+    # print(f'Fundamental matrix\n{F}')
     F_inv = np.linalg.inv(F)
-    print(f'Fundamental inv mat\n{F_inv}')
-    print(f'check\n{np.matmul(F, F_inv)}')
-    print(f'check\n{np.matmul(F_inv, F)}')
-    print(f'check\n{np.dot(F, F_inv)}')
-    print(f'check\n{np.dot(F_inv, F)}')
+    # print(f'Fundamental inv mat\n{F_inv}')
+    # print(f'matmul F, F_inv\n{np.matmul(F, F_inv)}')
+    # print(f'matmul F_inv, F\n{np.matmul(F_inv, F)}')
+    # print(f'dot F, F_inv\n{np.dot(F, F_inv)}')
+    # print(f'dot F_inv, F\n{np.dot(F_inv, F)}')
 
-    print('-'*50)
-    arr = np.array([
-        [[1,2,3],
-         [0,4,5],
-         [1,0,6]]])
-
-    arr_inv = np.linalg.inv(arr)
-
-    print(arr_inv)
-    print(np.dot(arr_inv,arr))
+    # # numpy inverse matrix 동작 확인
+    # print('-'*50)
+    # arr = np.array([
+    #     [[1,2,3],
+    #      [0,4,5],
+    #      [1,0,6]]])
+    #
+    # arr_inv = np.linalg.inv(arr)
+    #
+    # print(arr_inv)
+    # print(np.dot(arr_inv,arr))
 
 
 
