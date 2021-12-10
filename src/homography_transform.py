b63592bfa1cccaf8e785194862180b16cb00446c
deephigh <0525stone@gmail.com>
2021-12-10 10:45:34 +0900

    .

--- src/homography_transform.py
+++ src/homography_transform.py
@@ -35,8 +35,9 @@ def compute_orb_keypoints(filename):
     """
     # load image
     img = cv2.imread(filename)
-    img = cv2.pyrDown(img)
-    img = cv2.pyrDown(img)
+    # img = cv2.pyrDown(img)
+    # img = cv2.pyrDown(img)
+    print(f'image size : {img.shape}')
 
     # create orb object
     orb = cv2.ORB_create()
@@ -81,12 +82,13 @@ def compute_homography_matrix(filename1, filename2):
 
     # compute keypoint matches using descriptor
     matches = brute_force_matcher(des1, des2)
+    print(f'matches : {len(matches)}')
 
     # top-15 matches check
-    output_image = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10], None, flags=2)
-    # cv2.imshow('Output image', output_image)
-    # cv2.waitKey()
-    # cv2.destroyAllWindows()
+    output_image = cv2.drawMatches(img1, kp1, img2, kp2, matches[:30], None, flags=2)
+    cv2.imshow('Output image', output_image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
  # Fundamental matrices
     # extract points
@@ -126,60 +128,6 @@ def compute_homography_matrix(filename1, filename2):
     cv2.waitKey()
     cv2.destroyAllWindows()
 
-
-# fundamental matrix
-    # A = [[1,2],[3,4]]
-    # B = [1,7]
-    # print(A)
-    # print(np.dot(A,B))
-    # print(np.matmul(A, B))
-    #
-    # # match 된것 몇 개만 옮겨서 circle로 찍어보기
-    # print(f'matches count : {len(matches)}')
-    # for idx, p in enumerate(matches[:10]):
-    #     # print(f'kp {type(kp1)}, p {type(p)}')  # kp1 : tuple, p : cv2.DMatch
-    #     # print(f'point idx : {p.trainIdx}')
-    #     # print(f'point {kp1[p.queryIdx]}')
-    #     print(f'kp1 green {kp1[p.queryIdx].pt}')
-    #     print(f'kp2 red {kp2[p.trainIdx].pt}')
-    #
-    #     x1 , y1 = kp1[p.queryIdx].pt
-    #     x2, y2 = kp2[p.trainIdx].pt
-    #
-    # # # Matrix 형태1 : [[행][행]]
-    # #     x1n = int(F_inv[0][0] * x1 + F_inv[0][1] * y1 + F_inv[0][2])
-    #     # y1n = int(F_inv[1][0] * x1 + F_inv[1][1] * y1 + F_inv[1][2])
-    #     # x2n = int(F_inv[0][0] * x2 + F_inv[0][1] * y2 + F_inv[0][2])
-    #     # y2n = int(F_inv[1][0] * x2 + F_inv[1][1] * y2 + F_inv[1][2])
-    # # Matrix 형태2 : [[열][열]] -> 이게 맞음
-    #     x1n = int(F_inv[0][0] * x1 + F_inv[1][0] * y1 + F_inv[2][0])
-    #     y1n = int(F_inv[0][1] * x1 + F_inv[1][1] * y1 + F_inv[2][1])
-    #     x2n = int(F_inv[0][0] * x2 + F_inv[1][0] * y2 + F_inv[2][0])
-    #     y2n = int(F_inv[0][1] * x2 + F_inv[1][1] * y2 + F_inv[2][1])
-    #
-    #     x1ni = int(F[0][0] * x1 + F[1][0] * y1 + F[2][0])
-    #     y1ni = int(F[0][1] * x1 + F[1][1] * y1 + F[2][1])
-    #     x2ni = int(F[0][0] * x2 + F[1][0] * y2 + F[2][0])
-    #     y2ni = int(F[0][1] * x2 + F[1][1] * y2 + F[2][1])
-    #
-    #     x1,x2,y1,y2 = int(x1),int(x2),int(y1),int(y2)
-    #
-    #     print(f'x1 y1 x2 y2 {x1} {y1} {x2} {y2}')
-    #     print(f'inv x1 y1 x2 y2 {x1n} {y1n} {x2n} {y2n}')
-    #     print(f'f x1 y1 x2 y2 {x1ni} {y1ni} {x2ni} {y2ni}')
-    #
-    #     plt.plot(x1, y1, color='green', marker='o')
-    #     plt.plot(x2, y2, color='red', marker='o')
-    #     plt.plot([x1,x2], [y1,y2], color='blue')
-    #     # plt.plot(x1n, y1n, color='green', marker='v')
-    #     plt.plot(x2n, y2n, color='red', marker='v')
-
-
-
-
-        # print('-'*20)
-    # plt.show()
-
     return 0
 
 
@@ -189,31 +137,10 @@ def main():
     file_list = sorted(glob.glob(image_dir + '*.png'))
 
     # compute F matrix between two images
-    first_image_path = '../data/s09.jpeg'
-    second_image_path = '../data/s10.jpeg'
+    first_image_path = '../data/check2_.png' # s13, s15 : 비교적 선명한 damage car
+    second_image_path = '../data/check2.jpeg' # ss05
 
     H = compute_homography_matrix(first_image_path, second_image_path)
-    # print(f'Fundamental matrix\n{F}')
-    # F_inv = np.linalg.inv(F)
-    # F_inv = np.linalg.pinv(F)
-    # print(f'Fundamental inv mat\n{F_inv}')
-    # print(f'matmul F, F_inv\n{np.matmul(F, F_inv)}')
-    # print(f'matmul F_inv, F\n{np.matmul(F_inv, F)}')
-    # print(f'dot F, F_inv\n{np.dot(F, F_inv)}')
-    # print(f'dot F_inv, F\n{np.dot(F_inv, F)}')
-
-    # # numpy inverse matrix 동작 확인
-    # print('-'*50)
-    # arr = np.array([
-    #     [[1,2,3],
-    #      [0,4,5],
-    #      [1,0,6]]])
-    #
-    # arr_inv = np.linalg.inv(arr)
-    #
-    # print(arr_inv)
-    # print(np.dot(arr_inv,arr))
-
 
 
 if __name__ == '__main__':
