cc683fe4c1d881e8b52ab7a44e974c6d912b8048
0525stone <0525stone@gmail.com>
2021-12-17 14:41:51 +0900

    [DONE]tiny change

--- src/list_from_dir.py
+++ src/list_from_dir.py
@@ -9,7 +9,10 @@ def extract_name_dir(dir_name, savename):
     f = open(savename, 'w')
     img_list = os.listdir(dir_name)
     result = []
+    print(img_list)
+    img_list = sorted(img_list)
     for name in img_list:
+        # print(name)
         result.append(name)
         temp = name+'\n'
         f.write(temp)
@@ -17,18 +20,18 @@ def extract_name_dir(dir_name, savename):
     f.close()
 
 
-#
-# _root = '../data/side'
-# _clss = ['blur','scratch','good']
-# _cls = 'blur'
-# _file_dir = os.path.join(_root,_cls)
-# _save = 'img_'+_cls+'.txt'
-#
+
+_root = '../data/side'
+_clss = ['blur','scratch','good']
+_cls = 'blur'
+_file_dir = os.path.join(_root,_cls)
+_save = 'img_'+_cls+'.txt'
+
 # extract_name_dir(_file_dir, _save)
-#
-# # for c in _clss:
-# #     _file_dir = os.path.join(_root, c)
-# #     _save = 'img_' + c + '.txt'
-# #     extract_name_dir(_file_dir, _save)
-# #     # pass
+
+for c in _clss:
+    _file_dir = os.path.join(_root, c)
+    _save = 'img_' + c + '.txt'
+    extract_name_dir(_file_dir, _save)
+    # pass
 
