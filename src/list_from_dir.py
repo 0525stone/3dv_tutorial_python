e0e617965e44abc9aa527b0ee247bb8c27d0b057
0525stone <0525stone@gmail.com>
2021-12-17 14:00:36 +0900

    done

---
+++ src/list_from_dir.py
@@ -0,0 +1,34 @@
+"""
+2021.12.17
+from image directory make name list
+"""
+import os
+
+def extract_name_dir(dir_name, savename):
+
+    f = open(savename, 'w')
+    img_list = os.listdir(dir_name)
+    result = []
+    for name in img_list:
+        result.append(name)
+        temp = name+'\n'
+        f.write(temp)
+    print(f'length of image list : {len(result)}')
+    f.close()
+
+
+#
+# _root = '../data/side'
+# _clss = ['blur','scratch','good']
+# _cls = 'blur'
+# _file_dir = os.path.join(_root,_cls)
+# _save = 'img_'+_cls+'.txt'
+#
+# extract_name_dir(_file_dir, _save)
+#
+# # for c in _clss:
+# #     _file_dir = os.path.join(_root, c)
+# #     _save = 'img_' + c + '.txt'
+# #     extract_name_dir(_file_dir, _save)
+# #     # pass
+
