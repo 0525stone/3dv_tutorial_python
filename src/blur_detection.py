62357a3c92be8603636637b974562e8e252382cf
0525stone <0525stone@gmail.com>
2021-12-19 17:29:29 +0900

    .

--- src/blur_detection.py
+++ src/blur_detection.py
@@ -82,43 +82,43 @@ good_list = os.listdir(good_dir)
 
 # record_results()
 
-# # 근거가 있는지 확인하는 짧은 실험
-# for idx, (imagename, goodname) in enumerate(zip(image_list, good_list)):
-#     if idx<20:
-#         # print(image_path)
-#         image_path = os.path.join(image_dir, imagename)
-#         good_path = os.path.join(good_dir, goodname)
-#
-#         image = cv2.imread(image_path)
-#         image_good = cv2.imread(good_path)
-#         image = cv2.pyrDown(image)
-#         # image = cv2.pyrDown(image)
-#         # image_good = cv2.pyrDown(image_good)
-#         image_good = cv2.pyrDown(image_good)
-#         # image = cv2.pyrDown(image)
-#         # image_good = cv2.pyrDown(image_good)
-#
-#         # gray, gray_good = image, image_good
-#
-#         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#         gray_good = cv2.cvtColor(image_good, cv2.COLOR_BGR2GRAY)
-#         fm_good = variance_of_laplacian(gray_good)
-#         fm = variance_of_laplacian(gray)
-#
-#         text = "Not Blurry"
-#
-#         cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
-#                     cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-#         cv2.putText(image_good, "{}: {:.2f}".format(text, fm_good), (10, 30),
-#                     cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-#
-#         final_img = cv2.vconcat([image, image_good])
-#         cv2.imshow("Image", final_img)
-#         cv2.waitKey()
-#         cv2.destroyAllWindows()
-#
-#
-#     else:
-#         break
+# 근거가 있는지 확인하는 짧은 실험
+for idx, (imagename, goodname) in enumerate(zip(image_list, good_list)):
+    if idx<20:
+        # print(image_path)
+        image_path = os.path.join(image_dir, imagename)
+        good_path = os.path.join(good_dir, goodname)
+
+        image = cv2.imread(image_path)
+        image_good = cv2.imread(good_path)
+        image = cv2.pyrDown(image)
+        # image = cv2.pyrDown(image)
+        # image_good = cv2.pyrDown(image_good)
+        image_good = cv2.pyrDown(image_good)
+        # image = cv2.pyrDown(image)
+        # image_good = cv2.pyrDown(image_good)
+
+        # gray, gray_good = image, image_good
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray_good = cv2.cvtColor(image_good, cv2.COLOR_BGR2GRAY)
+        fm_good = variance_of_laplacian(gray_good)
+        fm = variance_of_laplacian(gray)
+
+        text = "Not Blurry"
+
+        cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+        cv2.putText(image_good, "{}: {:.2f}".format(text, fm_good), (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+
+        final_img = cv2.vconcat([image, image_good])
+        cv2.imshow("Image", final_img)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+
+
+    else:
+        break
 
 
