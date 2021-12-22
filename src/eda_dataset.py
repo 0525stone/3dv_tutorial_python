5ca882197f8d1f3637ebcd2979d78eb5b99a66b5
0525stone <0525stone@gmail.com>
2021-12-22 13:52:19 +0900

    first commit of eda code

---
+++ src/eda_dataset.py
@@ -0,0 +1,19 @@
+"""
+EDA
+
+When I first got Dataset, I want to know...
+- how many data are in the directory
+- how many folder are in dataset
+
+Image dataset
+- Distribution of image size
+- What image format is
+-
+"""
+
+
+
+
+
+
+
